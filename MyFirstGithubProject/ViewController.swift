9a86ccd85a1f846492868faf2819edb23e0837f6
Leogacho <levanigachechiladze1@gmail.com>
2024-02-12 17:08:10 +0400

    Initial Commit

---
+++ MyFirstGithubProject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyFirstGithubProject
+//
+//  Created by user on 21/11/1402 AP.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+  
+}
